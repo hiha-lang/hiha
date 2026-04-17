ddcaa67a0b5c141bb06f740c20c66924e40568ee
Barry Schwartz <150643+chemoelectric@users.noreply.github.com>
2026-04-17 13:18:19 -0500

    Make it pass ‘make distcheck’

---
+++ include/libhiha/libhiha.h
@@ -0,0 +1,39 @@
+/*
+
+  The hiha programming language
+
+  Copyright © 2026 Barry Schwartz
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef __LIBHAHA__LIBHIHA_H__INCLUDED__
+#define __LIBHAHA__LIBHIHA_H__INCLUDED__
+
+#include <libhiha/spinlock.h>
+#include <libhiha/initialize_once.h>
+#include <libhiha/string_t.h>
+#include <libhiha/token_t.h>
+#include <libhiha/pratt.h>
+#include <libhiha/load_plugin.h>
+
+#endif /* __LIBHAHA__LIBHIHA_H__INCLUDED__ */
+
+/*
+  local variables:
+  mode: c
+  coding: utf-8
+  end:
+*/

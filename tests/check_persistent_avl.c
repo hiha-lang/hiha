cc0e8343b83dd079872a5c50e695ede26d155de9
Barry Schwartz <150643+chemoelectric@users.noreply.github.com>
2026-05-09 07:16:42 -0500

    Added the persistent AVL test. Added the configure option for non-color tests.

---
+++ tests/check_persistent_avl.c
@@ -0,0 +1,37 @@
+/*
+
+  The hiha programming language
+
+  Copyright © 2026 Barry Schwartz
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+#include <config.h>
+#include <gc/gc.h>
+#include <libhiha/libhiha.h>
+
+HIHA_VISIBLE const char version_etc_copyright[] =
+  "Copyright %s %d Barry Schwartz";
+
+HIHA_AVL_TEST_DEFN (avl_test, 10000);
+
+int
+main (void)
+{
+  GC_INIT ();
+  avl_test ();
+  return 0;
+}
